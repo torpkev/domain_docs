60b9a8f76bcad80e350a0ae8772d0f68892ceafb
torpkev <kev@torp.work>
2021-03-29 11:01:28 -0500

    Delete package-search-index.js

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"work.torp.domain.api"},{"l":"work.torp.domain.api.events.block"},{"l":"work.torp.domain.api.events.domain"},{"l":"work.torp.domain.api.events.player"}]
